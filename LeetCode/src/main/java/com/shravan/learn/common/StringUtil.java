51310534df9e50345ac405c07410d0546e40dd6f
shravanshanmugam <shravan.shan@gmail.com>
2021-03-12 00:28:21 +0530

    leetcode easy interview string problems

--- LeetCode/src/main/java/com/shravan/learn/common/StringUtil.java
+++ LeetCode/src/main/java/com/shravan/learn/common/StringUtil.java
@@ -17,6 +17,16 @@ public class StringUtil {
 //         return (char) (a+'0');
     }
 
+    public static void reverse(char[] chars, int start, int end) {
+        while (start < end) {
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
     public static int[] lowerCaseFrequency(String word) {
         int[] frequency = new int[26];
         for (int i = 0; i < word.length(); i++) {
