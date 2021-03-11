ee328079ba7bdc133c3e4791dba9d55fdc71a42d
shravanshanmugam <shravan.shan@gmail.com>
2021-03-11 23:19:57 +0530

    leetcode easy interview array problems

---
+++ LeetCode/src/main/java/com/shravan/learn/common/ArrayUtil.java
@@ -0,0 +1,31 @@
+package com.shravan.learn.common;
+
+public class ArrayUtil {
+
+    public static void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static int[] remove(int[] array, int index) {
+        int[] copy = new int[array.length - 1];
+
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (i != index) {
+                copy[j++] = array[i];
+            }
+        }
+        return copy;
+    }
+}
