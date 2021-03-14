f14a45fd769eeedb30565923223971b4c81130da
shravanshanmugam <shravan.shan@gmail.com>
2021-03-14 16:09:38 +0530

    leetcode learning medium linkedlist problems

---
+++ LeetCode/src/main/java/com/shravan/learn/problems/medium/linkedlist/IntersectionOf2LinkedLists.java
@@ -0,0 +1,45 @@
+package com.shravan.learn.problems.medium.linkedlist;
+
+import com.shravan.learn.model.ListNode;
+
+public class IntersectionOf2LinkedLists {
+    // A = 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> NULL
+    // B = 9 -> 10 -> 3 -> 4 -> 5 -> 6 -> NULL
+    // intersection = 3
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        // compare node itself, not just value
+        while (tempA != tempB) {
+            tempA = tempA != null ? tempA.next : headB;
+            tempB = tempB != null ? tempB.next : headA;
+        }
+        return tempA;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode p = headA;
+        ListNode q = headB;
+        while (p != q) {
+            p = (p != null) ? p.next : headB;
+            q = (q != null) ? q.next : headA;
+        }
+        return p;
+
+    }*/
+}
