261242b34df1cdc58fac757f91ba5727fd5ea1e2
Shravan S <shravan.shan@nobroker.in>
2023-02-04 11:48:50 +0530

    move to new repo

--- LearnSpringBoot/src/main/java/com/shravan/learn/springboot/controllers/DummyController.java
+++ LearnSpringBoot/src/main/java/com/shravan/learn/springboot/controllers/DummyController.java
@@ -1,8 +1,13 @@
-package com.shravan.learn.frameworks.learnframeworks.concurrency;
+package com.shravan.learn.springboot.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.annotation.PreDestroy;
@@ -14,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 @RequestMapping("/dummy")
 public class DummyController {
 
-    private static ScheduledExecutorService delayer = Executors.newSingleThreadScheduledExecutor();
+    private static final ScheduledExecutorService delayer = Executors.newSingleThreadScheduledExecutor();
 
     @GetMapping("/ok")
     public ResponseEntity<String> returnOk() {
@@ -22,13 +27,13 @@ public class DummyController {
     }
 
     @PostMapping("/ok")
-    public ResponseEntity<String> returnOk(@RequestBody String body) {
+    public ResponseEntity<String> returnOk(@RequestBody final String body) {
         return new ResponseEntity<>(body, HttpStatus.OK);
     }
 
     @GetMapping("/ok/{delay}")
-    public DeferredResult<ResponseEntity<String>> delayOk(@PathVariable long delay) {
-        DeferredResult<ResponseEntity<String>> output = new DeferredResult<>(60_000L);
+    public DeferredResult<ResponseEntity<String>> delayOk(@PathVariable final long delay) {
+        final DeferredResult<ResponseEntity<String>> output = new DeferredResult<>(60_000L);
         delayer.schedule(() -> {
             output.setResult(ResponseEntity.ok("ok"));
         }, delay, TimeUnit.MILLISECONDS);
@@ -36,8 +41,8 @@ public class DummyController {
     }
 
     @PostMapping("/ok/{delay}")
-    public DeferredResult<ResponseEntity<String>> delayOk(@PathVariable long delay, @RequestBody String body) {
-        DeferredResult<ResponseEntity<String>> output = new DeferredResult<>(60_000L);
+    public DeferredResult<ResponseEntity<String>> delayOk(@PathVariable final long delay, @RequestBody final String body) {
+        final DeferredResult<ResponseEntity<String>> output = new DeferredResult<>(60_000L);
         delayer.schedule(() -> {
             output.setResult(ResponseEntity.ok(body));
         }, delay, TimeUnit.MILLISECONDS);
--- LeetCode/src/main/java/com/shravan/learn/common/CommonUtil.java
+++
@@ -1,7 +0,0 @@
-package com.shravan.learn.common;
-
-public class CommonUtil {
-    public static int sudokuBoxNumber(int row, int column) {
-        return (row / 3) * 3 + column / 3;
-    }
-}
